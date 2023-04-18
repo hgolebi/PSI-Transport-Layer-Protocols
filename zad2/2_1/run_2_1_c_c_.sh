96456d9d12a56a709f3644b62b000b2f950a75b9
Bartosz Pomiankiewicz <01159433@pw.edu.pl>
2023-04-18 21:56:37 +0200

    added decoration to scripts

--- zad2/2_1/run_2_1_c_c_.sh
+++ zad2/2_1/run_2_1_c_c_.sh
@@ -1,4 +1,10 @@
 docker run -dit --rm --network-alias z44_z21_c_server --network z44_network --name z44_z21_c_server z44_z21_c_server:latest
+echo ""
+echo "###### client ######"
+echo ""
 docker run -it --rm --network-alias z44_z21_c_client --network z44_network --name z44_z21_c_client z44_z21_c_client:latest z44_z21_c_server 2000
+echo ""
+echo "###### server ######"
+echo ""
 docker logs --tail 15 z44_z21_c_server
 docker kill z44_z21_c_server
