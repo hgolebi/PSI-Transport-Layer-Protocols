5555d8a06bd276efd1d6f362b45cb5a0dd4186dd
hgolebi1 <01161897@pw.edu.pl>
2023-04-19 00:25:56 +0200

    fixed bug in build for 2.3

--- zad2/2_3/build.sh
+++ zad2/2_3/build.sh
@@ -1,3 +1,3 @@
-docker build --tag z44_z23_c_client_block ../2_1/client_py
-docker build --tag z44_z23_c_client ./client_py
+docker build --tag z44_z23_c_client_block ../2_1/client_c
+docker build --tag z44_z23_c_client ./client_c
 docker build --tag z44_z23_py_server ./server_py
