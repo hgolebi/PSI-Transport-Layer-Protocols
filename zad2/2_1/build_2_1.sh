e190fe3fe9f66255f515c0105ee7fd4d2fcc0af8
Bartosz Pomiankewicz <01159433@pw.edu.pl>
2023-04-18 21:20:37 +0200

    scripts update

---
+++ zad2/2_1/build_2_1.sh
@@ -0,0 +1,4 @@
+docker build --tag z44_z21_c_client ./client_c
+docker build --tag z44_z21_c_server ./server_c
+docker build --tag z44_z21_py_client ./client_py
+docker build --tag z44_z21_py_server ./server_py
--- zad2/2_1/run_2_1_c_c_.sh
+++ zad2/2_1/run_2_1_c_c_.sh
@@ -1,4 +1,4 @@
-docker run -dit --rm --network-alias z44_z21_c_server --network z44_network --name z44_z21_c_server z44_z21_c_server
-docker run -it --rm --network-alias z44_z21_c_client --network z44_network --name z44_z21_c_client z44_z21_c_client
+docker run -dit --rm --network-alias z44_z21_c_server --network z44_network --name z44_z21_c_server z44_z21_c_server:latest
+docker run -it --rm --network-alias z44_z21_c_client --network z44_network --name z44_z21_c_client z44_z21_c_client:latest
 docker logs --tail 15 z44_z21_c_server
 docker stop z44_z21_c_server
