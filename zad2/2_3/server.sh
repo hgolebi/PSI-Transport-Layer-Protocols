f2e986a022e19945ffba12ef7aa67a86a5924549
hgolebi1 <01161897@pw.edu.pl>
2023-04-19 00:36:56 +0200

    server fix

--- zad2/2_3/server.sh
+++ zad2/2_3/server.sh
@@ -1,2 +1,2 @@
-docker run -it --rm --network-alias z44_z23_py_server --network z44_network --name z44_z23_py_server z44_z23_py_server:latest 10
+docker run -it --rm --network-alias z44_z23_py_server --network z44_network --name z44_z23_py_server z44_z23_py_server:latest
 
