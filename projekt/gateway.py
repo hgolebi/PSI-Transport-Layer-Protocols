990a800473f7256db9c76d89cc0ac72191b0cf48
Hubert <01161897@pw.edu.pl>
2023-05-30 20:26:32 +0200

    implemented non blocking sockets in gateway

--- projekt/gateway.py
+++ projekt/gateway.py
@@ -4,6 +4,7 @@ from command_interpreter import analyze_message, HELP_MESSAGE, EXIT_CODE
 from logger import Logger
 from udp_server import DeviceManager
 import threading
+import select
 
 HOST = socket.gethostbyname(socket.gethostname())
 PORT = 8000
@@ -13,8 +14,42 @@ START_MESSAGE = "Connected to the gateway." + HELP_MESSAGE
 CONN_CLOSED_MESSAGE = "Connection closed."
 # ERROR messages
 SEND_ERROR = "ERROR: Couldn't send the message."
+CONNECTION_ERROR = 'ERROR: Something went wrong during session.'
 
-buffer_size = 1024
+def client_service_thread(conn):
+    with conn:
+        conn.setblocking(False)
+        try:
+            select.select([], [conn], [], 5)
+            conn.send(START_MESSAGE.encode('UTF-8'))
+        except Exception as e:
+            logger.log(SEND_ERROR, CONN_CLOSED_MESSAGE)
+            logger.log(e)
+            return
+        while True:
+            try:
+                rl, _, _ = select.select([conn], [], [], 5)
+                if not rl:
+                    continue
+                data = conn.recv(1024)
+                if not data:
+                    break
+                message = data.decode('UTF-8')
+                logger.log(f"Client: {message}")
+                reply = analyze_message(message)
+                if reply == EXIT_CODE:
+                    conn.shutdown(socket.SHUT_RDWR)
+                    break
+                select.select([], [conn], [], 5)
+                conn.send(str(reply).encode('UTF-8'))
+            except Exception as e:
+                logger.log(CONNECTION_ERROR)
+                logger.log(e)
+                break
+        conn.close()
+    logger.log("Connection closed.")
+
+
 if len(argv) > 1 and argv[1].isdigit() and int(argv[1]) > 0:
     buffer_size = int(argv[1])
 
@@ -26,33 +61,17 @@ threading.Thread(None, DeviceManager.start, 'udp_server_thread', (HOST, UDP_PORT
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     s.bind((HOST, PORT))
-    s.listen()
+    s.setblocking(False)
+    s.listen(10)
     logger.log(f"Started listening on {HOST}:{PORT}")
     while True:
-        conn, addr = s.accept()
-        with conn:
+        try:
+            rl, _, _ = select.select([s], [], [], 5)
+            if not rl:
+                continue
+            conn, addr = s.accept()
             logger.log(f"Connected by {addr}")
-            try:
-                conn.send(START_MESSAGE.encode('UTF-8'))
-            except Exception as e:
-                logger.log(SEND_ERROR, CONN_CLOSED_MESSAGE)
-                logger.log(e)
-                break
-            while True:
-                try:
-                    data = conn.recv(buffer_size)
-                    if not data:
-                        break
-                    message = data.decode('UTF-8')
-                    logger.log(f"Client: {message}")
-                    reply = analyze_message(message)
-                    if reply == EXIT_CODE:
-                        conn.shutdown(socket.SHUT_RDWR)
-                        break
-                    conn.send(str(reply).encode('UTF-8'))
-                except Exception as e:
-                    logger.log(e)
-                    break
-            conn.close()
-        logger.log("Connection closed.")
-        logger.log(f"Listening on {HOST}:{PORT}")
+            threading.Thread(target=client_service_thread, args=[conn, ], daemon=True).start()
+        except Exception as e:
+            logger.log(e)
+            continue
